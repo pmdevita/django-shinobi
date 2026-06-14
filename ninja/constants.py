c01a943baae9d8c64609009b2acfb9544d480218
Peter DeVita <mewtwo2643@yahoo.com>
2026-06-14 18:31:39 -0400

    Update typing for 3.12, fix django mypy pre-commit deps (#83)

--- ninja/constants.py
+++ ninja/constants.py
@@ -10,7 +10,7 @@ class NOT_SET_TYPE:
     def __copy__(self) -> Any:
         return NOT_SET
 
-    def __deepcopy__(self, memodict: Optional[Dict] = None) -> Any:
+    def __deepcopy__(self, memodict: Optional[Dict[Any, Any]] = None) -> Any:
         return NOT_SET
 
 
